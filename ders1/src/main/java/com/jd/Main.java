45619c3bc11a478acabcbe0ad8de264f43d1ecb5
Suleyman Samet Yumusak <syumusak3@gmail.com>
2026-02-18 11:27:34 +0300

    git ignore ve ilk java projesi

---
+++ ders1/src/main/java/com/jd/Main.java
@@ -0,0 +1,8 @@
+package com.jd;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Dünya");
+        System.out.println("Hello world");
+    }
+}
