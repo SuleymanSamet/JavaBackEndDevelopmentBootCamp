44c5b3c091323e9335f9ea040b99714e472912d6
Suleyman Samet Yumusak <syumusak3@gmail.com>
2026-02-18 12:27:54 +0300

    5.Ders tamamlandı

---
+++ jd/src/main/java/com/jd/ders5/Main.java
@@ -0,0 +1,41 @@
+package com.jd.ders5;
+
+public class Main {
+    public static void main(String[] args) {
+        //variableType variableName= value;
+        int sayi1=10;
+        int sayi2=sayi1+20;
+
+        int s1=10;
+        int s2 =20;
+        int s3= s1+s2;
+        int s4=s2-s1;
+        int s5=s1*s2;
+        int s6=s2/s1;
+        int s7=s2%s1;
+        
+
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+        System.out.println(s3);
+        System.out.println(s2-s1);
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+        System.out.println(s7);
+        s1++;
+        s2--;
+        System.out.println(s1);
+        System.out.println(s2);
+        ++s1;
+        --s2;
+        System.out.println("++"+s1);
+        System.out.println("--"+s2);
+
+
+
+
+
+    }
+
+}
