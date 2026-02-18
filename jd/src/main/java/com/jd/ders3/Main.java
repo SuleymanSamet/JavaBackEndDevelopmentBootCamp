1fc3aed6e2c76708cdc125d6c58584940d476990
Suleyman Samet Yumusak <syumusak3@gmail.com>
2026-02-18 12:16:10 +0300

    3 ve 4. dersler tamamlandı

---
+++ jd/src/main/java/com/jd/ders3/Main.java
@@ -0,0 +1,26 @@
+package com.jd.ders3;
+
+public class Main {
+    public static void main(String[] args) {
+        byte s1=0;
+        short s2=1;
+        int s3=60000;
+        long s4=500000;
+        float s5=5.4f;
+        double s6=6.68;
+        boolean choise=true;
+        char c1='A';
+        String text="primitive türler";
+
+        System.out.println(text);
+        System.out.println(s1);
+        System.out.println(s2); 
+        System.out.println(s3); 
+        System.out.println(s4); 
+        System.out.println(s5); 
+        System.out.println(s6); 
+        System.out.println(choise); 
+        System.out.println(c1);     
+    }
+
+}
---
+++ jd/src/main/java/com/jd/ders4/Main.java
@@ -0,0 +1,24 @@
+package com.jd.ders4;
+
+public class Main {
+    public static final int NUMBER_MAX=100;
+    //int byte short boolean char long double float
+
+    //[veri tipinin anahtar kelimesi]+[variable/ değişken ismi]=[value/değer];
+    int vizeNot=70;
+    int vizenot=70;
+    //int vizeNot=80;
+    
+    //İsimlendirme kuralları 
+    /*
+    java büyük kucuk harfe duyarlıdır 
+    boşluk olamaz vize notu=100?
+    değişken isimlerini kullanırken kısaltma yerine tam isimlerini kullanın
+    finalNotu=100/fn=100;
+    java anahtar kelimelerini kullanamazsınız 
+    isimlendir kuralları camelCase kuralı ile kullan
+    final kullanılan verilerde hepsi büyük harf ve alt cizgi kullan
+
+    */
+
+}
